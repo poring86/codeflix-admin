75e40f2bdcc348dbb006f45c0fa9842862449738
Matheus Lino <matheuslino86@gmail.com.br>
2023-09-27 18:43:17 -0400

    feat: :sparkles: Theme and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { Box, ThemeProvider, createTheme } from '@mui/material'
+import { Box, ThemeProvider, Typography } from '@mui/material'
 import Header from './components/Header'
+import { appTheme } from './config/theme'
+import Layout from './components/Layout'
+import { Route, Routes } from 'react-router-dom'
 
-const theme = createTheme({})
+const About = () => <h1>About</h1>
+const Home = () => <h1>Home</h1>
 
 export default function App() {
   return (
 
-    <ThemeProvider theme={theme}>
-      <Box component="main" sx={{ height: "100vh", backgroundColor: "#000" }}>
+    <ThemeProvider theme={appTheme}>
+      <Box component="main" sx={{ height: "100vh", backgroundColor: (theme) => theme.palette.grey[900] }}>
         <Header />
+        <Layout>
+          <Typography variant="h1" component="h1">
+            News
+          </Typography>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/news" element={<About />} />
+          </Routes>
+        </Layout>
       </Box>
     </ThemeProvider>
   )
