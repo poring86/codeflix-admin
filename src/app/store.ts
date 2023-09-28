b738ea61734c172d13806dbb0a78435b8ae796a5
Matheus Lino <matheuslino86@gmail.com.br>
2023-09-28 17:45:57 -0400

    feat: :sparkles: Category slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import categorySlice from '../features/categories/categorySlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    categories: categorySlice
   },
 });
 
---
+++ src/features/categories/categorySlice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+interface Category {
+  id: string,
+  name: string,
+  description: string,
+  is_active: boolean,
+  delete_at: string | null,
+  created_at: string,
+  updated_at: string,
+}
+
+const category = {
+  id: "199183e2-5d97-11ee-8c99-0242ac120001",
+  name: "Olive",
+  description: "Earum quo at dolor tempore nisi.",
+  is_active: true,
+  delete_at: null,
+  created_at: "2020-06-06T11:00:00+0000",
+  updated_at: "2020-06-06T11:00:00+0000",
+}
+
+const categories = [
+  category,
+  { ...category, id: "199183e2-5d97-11ee-8c99-0242ac120002", name: "Peach" },
+  { ...category, id: "199183e2-5d97-11ee-8c99-0242ac120003", name: "Apple" },
+  { ...category, id: "199183e2-5d97-11ee-8c99-0242ac120003", name: "Banana" },
+]
+
+export const initialState = {
+  ...categories
+}
+
+const categoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {}
+})
+
+export default categoriesSlice.reducer
