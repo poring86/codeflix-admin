9c7e92f031ddf62a109abad21b16343611c00457
Matheus Lino <matheuslino86@gmail.com.br>
2023-09-29 09:29:51 -0400

    feat: :sparkles: Customize Datagrid fields

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useAppSelector } from '../../app/hooks'
 import { selectCategories } from './categorySlice'
-import { Box, Button, Typography } from '@mui/material'
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid'
+import { Box, Button, IconButton, Typography } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete'
+import { DataGrid, GridColDef, GridRenderCellParams, GridRowsProp } from '@mui/x-data-grid'
 
 export const ListCategory = () => {
   const categories = useAppSelector(selectCategories)
@@ -11,13 +12,39 @@ export const ListCategory = () => {
     id: category.id,
     name: category.name,
     description: category.description,
+    isActive: category.is_active,
+    createdAt: new Date(category.created_at).toLocaleDateString('pt-BR')
   }))
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: "Id", width: 150 },
-    { field: 'name', headerName: "Name", width: 150 },
-    { field: 'description', headerName: "Description", width: 150 },
+    { field: 'name', headerName: "Name", flex: 1 },
+    {
+      field: 'isActive', headerName: "Active", flex: 1, type: "boolean", renderCell: renderIsActiveCell
+    },
+    { field: 'description', headerName: "Description", flex: 1 },
+    {
+      field: 'createdAt', headerName: "Created At", flex: 1
+    },
+    {
+      field: 'id', headerName: "Actions", flex: 1, renderCell: renderActionCell
+    },
   ]
+
+  function renderActionCell(params: GridRenderCellParams) {
+    return (
+      <IconButton color="secondary" onClick={() => console.log("isActiveCell")}>
+        <DeleteIcon />
+      </IconButton>
+    )
+  }
+
+  function renderIsActiveCell(rowData: GridRenderCellParams) {
+    return (
+      <Typography color={rowData.value ? "primary" : "secondary"}>
+        {rowData.value ? "Active" : "Inactive"}
+      </Typography>
+    )
+  }
 
   return (
     <Box maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
