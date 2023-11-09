945eec4a7c81eda39ac708e44a79f1a14c06e5c5
Matheus Lino <matheuslino86@gmail.com.br>
2023-11-09 19:57:10 -0400

    feat: :sparkles: Cast member form tests

---
+++ src/features/castMembers/components/CastMemberForm.test.tsx
@@ -0,0 +1,42 @@
+import { render } from "@testing-library/react"
+import CastMemberForm from "./CastMemberForm"
+import { BrowserRouter } from "react-router-dom"
+
+const Props = {
+  castMember: {
+    id: "i",
+    name: "test",
+    type: 1,
+    deleted_at: null,
+    created_at: "2023-11-09T15:48:31+0000Z",
+    updated_at: "2023-11-09T15:48:31+0000Z",
+  },
+  isDisabled: false,
+  isLoading: false,
+  handleSubmit: jest.fn(),
+  handleChange: jest.fn(),
+}
+
+describe("CastMemberForm", () => {
+  it("should render castMember form correcty", () => {
+    const { asFragment } = render(<CastMemberForm  {...Props} />, {
+      wrapper: BrowserRouter
+    })
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("should render castmember form with loading state", () => {
+    const { asFragment } = render(<CastMemberForm {...Props} isLoading />, {
+      wrapper: BrowserRouter
+    })
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("should render castmember form with disabled state", () => {
+    const { asFragment } = render(<CastMemberForm {...Props} isLoading={true} isDisabled={true} />, {
+      wrapper: BrowserRouter
+    })
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
